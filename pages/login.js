6b2da5aa043401cf1d8d07da591db58ce9c0d32c
Alonso Peralta Espinoza <alonso.pe03@gmail.com>
2024-10-28 19:47:26 -0500

    Temp Login

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import styles from './login.module.css';
 
 const Login = () => {
+    const router = useRouter();
+
+    const CashierRoute = () => { router.push('/cashier'); }
+    const ManagerRoute = () => { router.push('./manager'); }
+
+
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className={styles.mainContainer}>
+            <h1>Choose Page</h1>
+            <button onClick={CashierRoute}>
+                Cashier View
+            </button> 
+            <button onClick={ManagerRoute}>
+                Manager View
+            </button>
         </div>
     )
 }
