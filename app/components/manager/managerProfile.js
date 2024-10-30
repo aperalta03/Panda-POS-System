3b55b24a1e564767f4dbfdb0ead4836625b36783
Anson <ansoncthai@gmail.com>
2024-10-30 16:40:32 -0500

    added styles for profile

--- app/components/manager/managerProfile.js
+++ app/components/manager/managerProfile.js
@@ -1,12 +1,13 @@
-// src/components/SalesReport.js
 import React, { useState } from 'react';
 import styles from './managerProfile.module.css';
 
 const ManagerProfile = () => {
     return (
     <div className={styles.mainContainer}>
-        <h2>Welcome Manager Name</h2>
-        {/* <img className={styles.profilePicture} src='blank-profile-picture.webp'/> */}
+        <div className={styles.managerName}>
+          <h2>Welcome Manager Name</h2>
+        </div>
+        <img className={styles.profilePicture} src='blank-profile-picture.webp'/>
     </div>
   );
 };
