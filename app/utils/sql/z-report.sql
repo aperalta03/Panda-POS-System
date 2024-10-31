07c6397def8e8db86dc460ad28923d2817e2282c
Alonso Peralta Espinoza <alonso.pe03@gmail.com>
2024-10-30 20:21:58 -0500

    Sales Report Done

--- app/utils/sql/z-report.sql
+++
@@ -1,3 +0,0 @@
--- z_report.sql
-INSERT INTO daily_sales_totals (sales_date, total_sales, total_orders)
-VALUES ($1, $2, $3);
--- pages/api/sales-report.js
+++ pages/api/sales-report.js
@@ -1,25 +1,26 @@
 import fs from 'fs';
 import path from 'path';
-import database from '../../app/utils/database';
+import database from '../../utils/database';
 
 export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        const { startDate, endDate } = req.query;
+  if (req.method === 'GET') {
+    const { startDate, endDate } = req.query;
 
-        try {
-            // Read the SQL file content
-            const filePath = path.join(process.cwd(), 'app/utils/sql/sales_report.sql');
-            const queryText = fs.readFileSync(filePath, 'utf-8');
+    try {
+      // Read the SQL file content
+      const filePath = path.join(process.cwd(), 'utils', 'sql', 'sales-report.sql');
+      console.log("SQL file path:", filePath); 
+      const queryText = fs.readFileSync(filePath, 'utf-8');
 
-            // Execute the query
-            const result = await database.query(queryText, [startDate, endDate]);
-            res.status(200).json({ data: result.rows });
-        } catch (error) {
-            console.error('Error fetching sales report data:', error);
-            res.status(500).json({ error: 'Error fetching sales report data' });
-        }
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
+      // Execute the query
+      const result = await database.query(queryText, [startDate, endDate]);
+      res.status(200).json({ data: result.rows });
+    } catch (error) {
+      console.error('Error fetching sales report data:', error);
+      res.status(500).json({ error: 'Error fetching sales report data' });
     }
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
