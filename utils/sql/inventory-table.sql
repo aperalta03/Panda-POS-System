17cd1fe414599ae68b7573232ee8bb13b36c91d5
andrewpopovici <andrewpopovici@tamu.edu>
2024-11-01 09:58:19 -0500

    Updated Inventory Table SQL Query
    Query does not touch menu-inventory junction, just fetches all relevant inventory data.

--- utils/sql/inventory-table.sql
+++ utils/sql/inventory-table.sql
@@ -1,11 +1,11 @@
 -- inventory-table.sql
 SELECT 
-    mi.menu_item_id AS id,
-    i.item_name AS name,
-    i.curr_amount AS stocked,
-    i.needed4week AS required,
-    (i.needed4week - i.curr_amount) AS to_order
+    inventory_id AS id,
+    item_name,
+    curr_amount AS stocked,
+    needed4week AS baseline,
+    (needed4week - curr_amount) AS to_order
 FROM 
-    menu_inventory mi
-JOIN 
-    inventory i ON mi.inventory_id = i.inventory_id;
+    inventory
+ORDER BY
+    inventory_id
