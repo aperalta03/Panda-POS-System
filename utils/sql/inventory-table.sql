a235c23a1fcab355f83a177e91ad9992b63869bc
andrewpopovici <156961408+andrewpopovici@users.noreply.github.com>
2024-11-04 10:22:40 -0600

    Update inventory-table.sql
    Fixed inventory table display on manager page.

--- utils/sql/inventory-table.sql
+++ utils/sql/inventory-table.sql
@@ -1,11 +1,11 @@
 -- inventory-table.sql
 SELECT 
     inventory_id AS id,
-    item_name,
+    item_name AS name,
     curr_amount AS stocked,
-    needed4week AS baseline,
+    needed4week AS required,
     (needed4week - curr_amount) AS to_order
 FROM 
     inventory
 ORDER BY
-    inventory_id
+    inventory_id;
