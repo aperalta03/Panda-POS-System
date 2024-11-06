db717243ccfb2054e47c340f7b80e33ed06aac75
Alonso Peralta Espinoza <alonso.pe03@gmail.com>
2024-11-06 17:19:50 -0600

    COmmit Farming 2

--- pages/api/sales-chart.js
+++ pages/api/sales-chart.js
@@ -41,18 +41,3 @@ export default async function handler(req, res) {
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
-
-// SELECT
-// s.date_of_sale,
-//     s.time_of_sale,
-//     i.item_name
-// FROM
-//                     sales s
-//                     JOIN sales_menu sm ON s.sale_number = sm.sale_number
-//                     JOIN menu_inventory mi ON sm.menu_item_id = mi.menu_item_id
-//                     JOIN inventory i ON mi.inventory_id = i.inventory_id
-// WHERE
-// i.item_name = 'Chow Mein' AND
-// s.date_of_sale BETWEEN $2 AND $3
-//                 ORDER BY
-// s.date_of_sale ASC, s.time_of_sale ASC;
