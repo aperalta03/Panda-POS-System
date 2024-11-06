ac550492b35084712f2add4043831f7ad8bd2da7
Conner <connersb@tamu.edu>
2024-11-06 10:03:32 -0600

    fixing sql statements for z report

---
+++ utils/sql/z-report-sales.sql
@@ -0,0 +1,18 @@
+INSERT INTO sales
+    (
+        sale_number,
+        date_of_sale,
+        employee_id,
+        time_of_sale,
+        price,
+        franchise_id
+    )
+VALUES
+    (
+        (SELECT MAX(sale_number) + 1 FROM sales),
+        $1,
+        $2,
+        $3,
+        $4,
+        $5
+    );
