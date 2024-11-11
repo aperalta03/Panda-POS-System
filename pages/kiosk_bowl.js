a2f3d9cb7a350d59b2a46c961b876b42fdf9ab5b
uzairak12 <uzairak12@tamu.edu>
2024-11-11 13:10:12 -0600

    Created pages for kiosk, filled out kiosk_item file and functionality.

---
+++ pages/kiosk_bowl.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import styles from './kiosk_bowl.module.css';
+import { useRouter } from "next/router";
+
+const TopBar = ({handleCartClick}) => {
+    const router = useRouter();
+    return (
+        <div className = {styles.KioskItemPanel}>
+            <div className = {styles.leftTopPanel}>
+                <h1 className = {styles.plateName}>Bowl</h1>
+                <h3 className = {styles.selectLabel}>Select:</h3>
+                <h3 className = {styles.sideLabel}>1 Side</h3>
+                <h3 className = {styles.entreeLabel}>1 Entree</h3>
+            </div>
+            <div className = {styles.rightTopPanel}>
+                <button className = {styles.circleButton} onClick={handleCartClick}>
+                🛒
+                </button>
+                <h2 className = {styles.sideQuant}>0.5</h2>
+                <h2 className = {styles.sideQuantLabel}>Side</h2>
+                <h2 className = {styles.entreeQuant}>2</h2>
+                <h2 className = {styles.entreeQuantLabel}>Entree</h2>
+            </div>
+        </div>
+    );
+};
+
+const KioskBowlPage = () => {
+    const router = useRouter();
+    
+    const handleCartClick = () => {
+        router.push("/kiosk_item");
+    };
+
+    return (
+        <div className= {styles.layout}>
+            <div className= {styles.topHeader}>
+                <TopBar>
+                    handleCartClick = {handleCartClick}
+                </TopBar>
+            </div>
+        </div>
+    );
+};
+
+export default KioskBowlPage;
