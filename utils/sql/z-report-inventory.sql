4842c84c81753be11b4b46568e524b840b8c0876
Alonso Peralta Espinoza <alonso.pe03@gmail.com>
2024-11-13 10:34:32 -0600

    Fixed Z-Report

--- utils/sql/z-report-inventory.sql
+++ utils/sql/z-report-inventory.sql
@@ -1,5 +1,21 @@
-SELECT si.itemname AS item_name, COUNT(si.itemname) AS quantity_sold, SUM(sr.totalprice) AS total_price
-FROM saleItems si
-JOIN salesRecord sr ON si.salenumber = sr.salenumber
-GROUP BY si.itemname
-ORDER BY quantity_sold DESC;
+WITH item_sales AS (
+    SELECT 
+        jsonb_array_elements_text(si.components) AS item_name,
+        COUNT(*) AS quantity_sold,
+        SUM(sr.totalprice / jsonb_array_length(si.components)) AS total_price
+    FROM 
+        saleItems si
+    JOIN 
+        salesRecord sr ON si.salenumber = sr.salenumber
+    GROUP BY 
+        item_name
+)
+SELECT 
+    item_name,
+    quantity_sold,
+    ROUND(total_price, 2) AS total_price,
+    (SELECT ROUND(SUM(total_price), 2) FROM item_sales) AS total_sales
+FROM 
+    item_sales
+ORDER BY 
+    quantity_sold DESC;
