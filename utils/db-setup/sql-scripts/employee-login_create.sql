6daabd3b4c94880a4b270c02b4d9d09ec3f48227
andrewpopovici <andrewpopovici@tamu.edu>
2024-11-20 20:31:01 -0600

    Revert "Merge branch 'main' of https://github.com/CSCE331-Fall2024/project-3-61-chicken-maxing"
    This reverts commit 8f7e80f433b60aaa7bc36d59d98124551d8a6b25, reversing
    changes made to be1e8f2cf8d2aa58ef86e34247df00a610b63c71.

--- utils/db-setup/sql-scripts/employee-login_create.sql
+++ utils/db-setup/sql-scripts/employee-login_create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE employee_login (
-    employee_id INT,
-    email VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL,
+    employee_id INT REFERENCES employees(employee_id) ON DELETE SET NULL,
+    email VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL
     verified BOOLEAN DEFAULT false
 );
