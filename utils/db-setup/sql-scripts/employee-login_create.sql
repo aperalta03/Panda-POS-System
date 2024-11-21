13f45ecd4cc38a156d0f91654f676b48eec31adf
Alonso Peralta Espinoza <alonso.pe03@gmail.com>
2024-11-20 18:30:08 -0600

    Fixed Employee Login Table

--- utils/db-setup/sql-scripts/employee-login_create.sql
+++ utils/db-setup/sql-scripts/employee-login_create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employee_login (
-    employee_id INT PRIMARY KEY REFERENCES employees(employee_id) ON DELETE CASCADE,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    employee_id INT REFERENCES employees(employee_id) ON DELETE SET NULL,
+    email VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
     password VARCHAR(255) NOT NULL
 );
