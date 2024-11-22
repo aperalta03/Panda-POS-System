77a5a047e7014ac8b08f8a45a7d109ffc973173c
Alonso Peralta Espinoza <alonso.pe03@gmail.com>
2024-11-21 20:54:09 -0600

    AI Agent Added to Kiosk Views

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../app/globals.css";
 import styles from "./_app.module.css";
 import Navbar from "../app/components/navbar/navbar";
+import AiAgent from "@/app/components/agent/ai-agent";
 import { RoleProvider } from "../app/context/roleProvider";
 import { UserProvider } from "../app/context/userProvider";
 import { TranslationProvider } from "@/app/context/translateContext";
 import { useRouter } from "next/router";
 import { GlobalStateProvider } from "../app/context/GlobalStateContext";
+
 function MyApp({ Component, pageProps }) {
   return (
     <RoleProvider>
@@ -32,12 +34,23 @@ const Layout = ({ children }) => {
     router.pathname === "/kitchenTV" ||
     router.pathname === "/menu";
 
+  const showAI =
+    router.pathname === "/kiosk" ||
+    router.pathname === "/kiosk_item" ||
+    router.pathname === "/kiosk_bowl" ||
+    router.pathname === "/kiosk_plate" ||
+    router.pathname === "/kiosk_bigger_plate" ||
+    router.pathname === "/kiosk_a_la_carte";
+
   return (
     <div className={styles.layoutContainer}>
       {showNavbar && (
         <div className={styles.navbarContainer}>
           <Navbar />
         </div>
+      )}
+      {showAI && (
+        <AiAgent />
       )}
       <div className={styles.pageContent}>{children}</div>
     </div>
