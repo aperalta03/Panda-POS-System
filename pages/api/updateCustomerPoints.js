484bce0d31dbb804bf32555cf775bf5e9309f137
Conner <connersb@tamu.edu>
2024-11-29 18:03:06 -0600

    Fixing an issue with the api to update points

--- pages/api/updateCustomerPoints.js
+++ pages/api/updateCustomerPoints.js
@@ -3,30 +3,30 @@ import fs from 'fs';
 import path from 'path';
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { phoneNumber, points } = req.body;
-    
-    if(!phoneNumber){
-        return res.status(400).json({ error: 'Missing required fields: phone number' });
-    }
+    if (req.method === 'POST') {
+        const { phoneNumber, points } = req.body;
+        
+        if(!phoneNumber){
+            return res.status(400).json({ error: 'Missing required fields: phone number' });
+        }
 
-    try{
-        const filePath = path.join(process.cwd(), 'utils', 'sql', 'update-customer-points.sql');
-        const insertScript = fs.readFileSync(filePath, 'utf8');
+        try{
+            const filePath = path.join(process.cwd(), 'utils', 'sql', 'update-customer-points.sql');
+            const insertScript = fs.readFileSync(filePath, 'utf8');
 
-        const values = [points, phoneNumber];
+            const values = [points, phoneNumber];
 
-        const result = await database.query(queryText, values);
+            const result = await database.query(queryText, values);
 
-        res.status(200).json({ message: 'Points updated sucessfully successfully', employee: result.rows[0] });
-    }
-    catch (error) {
-        // Log the error (for debugging) but do not crash the app
-        console.error('Error updating points:', error);
+            res.status(200).json({ message: 'Points updated sucessfully successfully', employee: result.rows[0] });
+        }
+        catch (error) {
+            // Log the error (for debugging) but do not crash the app
+            console.error('Error updating points:', error);
 
-        // Send a generic error message to the user
-        res.status(400).json({ error: 'Bad input. Please check your data and try again.' });
-    }
+            // Send a generic error message to the user
+            res.status(400).json({ error: 'Bad input. Please check your data and try again.' });
+        }
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
